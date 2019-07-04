56b86670a159ce881d89c6fe2ba0d01f51bd1841
Hai NT <hai.nt@teko.vn>
2019-07-04 17:29:18 +0700

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -2,7 +2,7 @@ local typedefs = require "kong.db.schema.typedefs"
 
 
 return {
-  name = "decision-makers",
+  name = "decision-maker",
   fields = {
     { protocols = typedefs.protocols_http },
     {
