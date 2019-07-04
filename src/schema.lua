e201c428e41695860c87aa12dbbf46f7185e79e6
Hai NT <hai.nt@teko.vn>
2019-07-04 17:26:54 +0700

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -4,9 +4,17 @@ local typedefs = require "kong.db.schema.typedefs"
 return {
   name = "decision-maker",
   fields = {
-
+    { protocols = typedefs.protocols_http },
+    {
+      config = {
+        type = "record",
+        fields = {
+          { decision_maker = {
+            type = "foreign",
+            reference = "decision_makers",
+            default   = ngx.null,
+            on_delete = "cascade",
+          }, }
+    }, }, },
   },
-  entity_checks = {
-
-  }
 }
