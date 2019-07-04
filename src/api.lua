3ceefa17b7b218b3dd6bf694c086d30454936a02
Hai NT <hai.nt@teko.vn>
2019-07-04 16:01:21 +0700

    Update admin API

--- src/api.lua
+++ src/api.lua
@@ -3,10 +3,11 @@ local decision_makers_schema = kong.db.decision_makers.schema
 
 
 return {
-  ["/decision-makers/"] = {
+  ["/decision-makers"] = {
     schema = decision_makers_schema,
     methods = {
-
+      GET = endpoints.get_collection_endpoint(decision_makers_schema),
+      POST = endpoints.post_collection_endpoint(decision_makers_schema)
     }
   }
 }
