4a836262a384e2edae1df593e1b1a533a994a676
Hai NT <hai.nt@teko.vn>
2019-07-04 14:21:36 +0700

    Update migrations

---
+++ src/migrations/000_base_decision_maker.lua
@@ -0,0 +1,38 @@
+return {
+  postgres = {
+    up = [[
+      CREATE TABLE IF NOT EXISTS "decision_makers" (
+        "id"           UUID                         PRIMARY KEY,
+        "identifier"   TEXT                         UNIQUE,
+        "created_at"   TIMESTAMP WITHOUT TIME ZONE  DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'UTC'),
+        "scheme"       TEXT,
+        "host"         TEXT,
+        "port"         INTEGER,
+        "path"         TEXT
+      );
+
+      DO $$
+      BEGIN
+        CREATE INDEX IF NOT EXISTS "decision_makers_host_idx" ON "decision_makers" ("host");
+      EXCEPTION WHEN UNDEFINED_COLUMN THEN
+        -- Do nothing, accept existing state
+      END$$;
+    ]],
+  },
+
+  cassandra = {
+    up = [[
+      CREATE TABLE IF NOT EXISTS decision_makers(
+        id          uuid PRIMARY KEY,
+        identifier  text,
+        created_at  timestamp,
+        scheme      text,
+        host        text,
+        port        bigint,
+        path        text
+      );
+      CREATE INDEX IF NOT EXISTS ON decision_makers(identifier);
+      CREATE INDEX IF NOT EXISTS ON decision_makers(host);
+    ]],
+  }
+}
