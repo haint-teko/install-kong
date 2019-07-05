fb23d206139bb6d36a1500184961a4c7571d8a31
Hai NT <hai.nt@teko.vn>
2019-07-05 15:25:34 +0700

    Update handler.lua

--- src/handler.lua
+++ src/handler.lua
@@ -15,40 +15,56 @@ function DecisionMakerHandler:new()
 end
 
 
+local function connect(client, host, port)
+  local ok, err = client:connect(host, port)
+  if not ok then
+    local domain = host .. ":" .. port
+    kong.log.err("Could not connect to decision-maker service " .. domain ": " .. err)
+    return false
+  end
+  return true
+end
+
+
 local function make_decision(conf)
-  local client = http.new()
   local request_method = kong.request.get_method()
   local request_path = kong.request.get_path_with_query()
   local request_body = kong.request.get_raw_body()
   local request_headers = kong.request.get_headers()
   local service = kong.router.get_service()
   request_headers["X-Upstream-Host"] = service.host .. ":" .. service.port
+  local client = http.new()
+  client:set_timeout(conf.timeout)
 
-  local connected = false
-  local decision_maker_path = "/decisions"
-  for decision_maker in kong.db.decision_makers:each() do
-    client:set_timeout(500)
-    local ok, err = client:connect(decision_maker.host, decision_maker.port)
+  local decision_maker
+  if conf.decision_maker ~= nil then
+    decision_maker = kong.db.decision_makers:select(conf.decision_maker)
+    local ok = connect(client, decision_maker.host, decision_maker.port)
     if not ok then
-      local domain = decision_maker.host .. ":" .. decision_maker.port
-      kong.log.err("Could not connect to decision-maker service " .. domain .. ": " .. err )
-    else
-      if decision_maker.path ~= nil then
-        decision_maker_path = decision_maker.path
-      end
-      connected = true
-      break
+      decision_maker = nil
     end
   end
 
-  if connected == false then
+  if conf.decision_maker == nil or decision_maker == nil then
+    for another in kong.db.decision_makers:each() do
+      local ok = connect(client, another.host, another.port)
+      if ok then
+        decision_maker = another
+        break
+      end
+    end
+  end
+
+  if decision_maker == nil then
     kong.log.err("Could not connect to any decision-maker service")
     return kong.response.exit(HTTP_500, { message = "An unexpected error occurred" })
   end
 
+  local path = decision_maker.path or "/decisions"
+  request_path = path .. request_path
   local res, err = client:request {
     method = request_method,
-    path = decision_maker_path .. request_path,
+    path = request_path,
     body = request_body,
     headers = request_headers
   }
@@ -58,9 +74,15 @@ local function make_decision(conf)
     return kong.response.exit(HTTP_500, { message = "An unexpected error occurred" })
   end
 
-  local response_status = res.status
-  local response_content = res:read_body()
-  return response_status, response_content
+  local status_code = res.status
+  local content = res:read_body()
+
+  res, err = client:set_keepalive(conf.keepalive)
+  if not res then
+    kong.log.err("Could not keepalive connection: " .. err)
+  end
+
+  return status_code, content
 end
 
 
