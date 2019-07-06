0adb02da23215fab69c4b5abd549eb228a81eb85
Hai NT <hai.nt@teko.vn>
2019-07-06 11:39:32 +0700

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -10,20 +10,9 @@ return {
       config = {
         type = "record",
         fields = {
-          { decision_maker = {
-            type = "foreign",
-            reference = "decision_makers",
-            default   = ngx.null,
-            on_delete = "cascade",
-          }, },
-          { timeout = {
-            type = "number",
-            default = 60000,
-          }, },
-          { keepalive = {
-            type = "number",
-            default  = 60000
-          }, },
+          { decision_maker = { type = "foreign", reference = "decision_makers", default   = ngx.null, on_delete = "cascade", }, },
+          { timeout = { type = "number", default = 60000, }, },
+          { keepalive = { type = "number", default  = 60000 }, },
     }, }, },
   },
 }
