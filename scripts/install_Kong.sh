32b1fda5d1cb9b2f09d5dc25b3bd64fa320f9019
Hai NT <hai.nt@teko.vn>
2019-07-10 16:28:02 +0700

    Update install_Kong.sh

--- scripts/install_Kong.sh
+++ scripts/install_Kong.sh
@@ -92,7 +92,9 @@ function install_kong() {
   prepare_env
 
   apt-get install -y openssl libpcre3 procps perl
+  wait
   dpkg -i ./packages/kong-1.2.1.*.deb
+  wait
 
   mv kong.conf /etc/kong/kong.conf && chmod 644 /etc/kong/kong.conf
   cp ./templates/custom_nginx.template /etc/kong && chmod 644 /etc/kong/custom_nginx.template
