dc1be27e6ced3db16779dcef826519ee83d155d2
Hai NT <hai.nt@teko.vn>
2019-07-11 09:51:26 +0700

    Update install_decision_maker.sh

--- scripts/install_decision_maker.sh
+++ scripts/install_decision_maker.sh
@@ -9,7 +9,7 @@ luarocks make
 cd ../..
 
 PLUGINS=$(egrep "^\s*plugins" /etc/kong/kong.conf)
-sed -i "s|^\s*plugins\s*=\s*.*$|${PLUGINS}, ${PLUGIN_NAME}"
+sed -i "s|^\s*plugins\s*=\s*.*$|${PLUGINS}, ${PLUGIN_NAME}|"
 systemctl reload kong
 
 /usr/local/bin/kong migrations bootstrap up --conf /etc/kong/kong.conf
