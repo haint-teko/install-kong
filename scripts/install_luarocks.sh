e01337094000cd54d621b2a58331906e42b85dbe
Hai NT <hai.nt@teko.vn>
2019-07-12 14:05:57 +0700

    Add install_luarocks.sh

---
+++ scripts/install_luarocks.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+apt-get install -y \
+    build-essential \
+    libreadline-dev \
+    wget
+
+wget https://luarocks.github.io/luarocks/releases/luarocks-2.4.3.tar.gz
+tar -zxf luarocks-2.4.3.tar.gz
+
+cd ./luarocks-2.4.3
+
+./configure \
+    --lua-suffix=jit \
+    --with-lua=/usr/local/openresty/luajit \
+    --with-lua-include=/usr/local/openresty/luajit/include/luajit-2.1
+make build
+make install
+
+rm -rf luarocks-2.4.3*
